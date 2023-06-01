fe5f8566eacf16f479685fccdbc83da0fd9d9c9f
Lucas Teles <lucas.teles@outlook.com>
2023-06-01 10:58:40 -0300

    plain api

---
+++ CryptoMidSample.Tests/ApiTests.cs
@@ -0,0 +1,24 @@
+namespace CryptoMidSample.Tests;
+
+public class ApiTests : IDisposable
+{
+    readonly WebApplicationFactory<Program> app = new();
+    readonly HttpClient client;
+    readonly Faker faker = new();
+
+    public ApiTests()
+    {
+        client = app.CreateClient();
+        Randomizer.Seed = new(42);
+    }
+
+    public void Dispose() => app.Dispose();
+
+    [Fact]
+    public async Task ShouldGetEncrypted()
+    {
+        var name = faker.Name.FirstName();
+        var response = await client.GetStringAsync($"/hello/{name}");
+        response.Should().Be($"Hello {name}!");
+    }
+}
